91c68f1128808f35895b0e35ddea25c3f95341c8
ace90210 <wright.barry@gmx.com>
2013-01-31 14:10:03 +0000

    updated ball added

--- src/com/badlogic/androidgames/gamedev2d/FontTest.java
+++ src/com/badlogic/androidgames/gamedev2d/FontTest.java
@@ -2,10 +2,14 @@ package com.badlogic.androidgames.gamedev2d;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import com.badlogic.androidgames.framework.DynamicGameObject;
 import com.badlogic.androidgames.framework.Game;
+import com.badlogic.androidgames.framework.GameObject;
 import com.badlogic.androidgames.framework.Screen;
 import com.badlogic.androidgames.framework.gl.Camera2D;
 import com.badlogic.androidgames.framework.gl.SpriteBatcher;
+import com.badlogic.androidgames.framework.gl.Texture;
+import com.badlogic.androidgames.framework.gl.TextureRegion;
 import com.badlogic.androidgames.framework.impl.GLGame;
 import com.badlogic.androidgames.framework.impl.GLGraphics;
 import com.badlogic.androidgames.myitems.GLText;
@@ -23,18 +27,27 @@ public class FontTest extends GLGame {
 		GLGraphics glGraphics;
 		GL10 gl;
 		GLText glText;
+		DynamicGameObject ball;
+		Texture texture;
+		TextureRegion ballRegion;
+		
 		
 		SpriteBatcher batcher;
 		
 		public FontScreen(Game game) {
 			super(game);
-			
+			ball = new DynamicGameObject(0, 0, 0.2f, 0.2f);
 			glGraphics = ((GLGame)game).getGLGraphics();
 			gl = glGraphics.getGL();			
 			camera = new Camera2D(glGraphics, WORLD_WIDTH, WORLD_HEIGHT);
 			batcher = new SpriteBatcher(gl, 100);	
 			
-			 glText = new GLText( gl, ((GLGame)game).getAssets() );
+			texture = new Texture(((GLGame)game), "atlas.png");
+			
+			ballRegion = new TextureRegion(texture, 0, 32, 16, 16);
+			
+			
+			glText = new GLText( gl, ((GLGame)game).getAssets() );
 
 		     glText.load( "ARIAL.TTF", 14, 2, 2 );  
 		}
@@ -70,6 +83,10 @@ public class FontTest extends GLGame {
 	        glText.draw( "More Lines...", 50, 150 );        // Draw Test String
 	        glText.draw( "The End.", 50, 150 + glText.getCharHeight() );  // Draw Test String
 	        glText.end();   
+	        
+	        batcher.beginBatch(texture);			
+			batcher.drawSprite(ball.position.x, ball.position.y, 50f, 50.0f, ballRegion);
+			batcher.endBatch();
 			
 		}
 
